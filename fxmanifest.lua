0b0bf33be7ed40abdfbf80fe6bed7cff946a25ea
Mfrouk <115092889+Mfrouk@users.noreply.github.com>
2023-01-01 16:19:48 +0100

    Update fxmanifest.lua
    removed unnecessary things from fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,28 +9,8 @@ version '1.0.0'
 
 lua54 'yes'
 
-shared_scripts {
-	'@ox_lib/init.lua',
-	'shared/sh_config.lua',
-}
-
-client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/CircleZone.lua',
-	'client/*.lua',
-}
 
 server_scripts {
 	'server/*.lua',
 }
 
-files {
-	'locales/*.json'
-}
-
-escrow_ignore {
-	'shared/*.lua',
-	'client/cl_Utils.lua',
-	'server/sv_Utils.lua',
-}
